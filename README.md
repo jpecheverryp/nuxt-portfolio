40a1c66078fb4f04d09bb96f34d546e7e4accd1e
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-05-12 18:38:45 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Nuxt Portfolio
+
+Hi there!
+My personal Site 3.0 using [NuxtJS](https://nuxtjs.org/)
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello!</h1>
+    <h2>My name is Juan. I'm a Software Developer</h2>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
