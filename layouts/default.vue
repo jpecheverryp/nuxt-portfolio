30d8bda015012a4495b5bcb1a684dc2c05e55e80
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-05-12 18:49:47 -0400

    add pages to directory

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <a href="#">Hey</a>
+    <nuxt-link to="/">Hey</nuxt-link>
     <nav>
       <ul>
         <nuxt-link to="/dev"><li>Dev</li></nuxt-link>
+        <a href="https://github.com/jpecheverryp/nuxt-portfolio" target="_blank"><li>Source</li></a>
         <nuxt-link to="/contact"><li>Contact</li></nuxt-link>
       </ul>
     </nav>
