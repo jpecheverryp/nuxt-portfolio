e60e9025e03ab95a9ef69bc1bf6cb387b2ba290f
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-05-14 00:53:46 -0400

    add image and some dummy text to blog

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <img :src="article.img" :alt="article.alt">
+    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+
     <nuxt-content :document="article" />
   </div>
 </template>
 
 <script>
   export default {
+    methods: {
+      formatDate(dateStr) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' }
+        return new Date(dateStr).toLocaleString('en', options)
+      }
+    },
     async asyncData({ $content, params }) {
       const article = await $content('articles', params.slug).fetch()
 
