74ab95e8725a26b51b03deb185affe9db944af4d
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-05-15 17:57:10 -0400

    add hero image

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>I bring ideas to reality</h1>
     <h2>My name is Juan<br> I'm a Web Developer <br/>and Web Designer</h2>
-    
+    <img src="~/static/svg/mobile-wireframe.svg" alt="">
   </div>
 </template>
 
