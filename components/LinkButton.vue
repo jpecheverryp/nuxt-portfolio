e934fae2ffa47ad23799c8c57164d1d1695bff65
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-05-15 23:01:53 -0400

    styled heading, and buttons in hero section

--- components/LinkButton.vue
+++ components/LinkButton.vue
@@ -1,9 +1,9 @@
 <template>
 <div v-if="isExternal">
-  <a class="btn" :href="address" target="_blank">{{text}}</a>
+  <a :class="[isGhost ? 'is-ghost' : '', 'btn']" :href="address" target="_blank">{{text}}</a>
 </div>
 <div v-else>
-  <nuxt-link class="btn" :to="address">{{text}}</nuxt-link>
+  <nuxt-link :class="[isGhost ? 'is-ghost' : '', 'btn']" :to="address">{{text}}</nuxt-link>
 </div>
 </template>
 
@@ -23,9 +23,28 @@
         default: false,
         type: Boolean,
         required: false
+      },
+      isGhost: {
+        default: false,
+        type: Boolean,
+        required: false
       }
     }
   }
 </script>
 
-<style scoped</style>
+<style scoped>
+  .btn {
+    text-decoration: none;
+    background-color: #3CAF81;
+    color: black;
+    padding: 0.875rem 1.125rem;
+    border-radius: 0.625rem;
+    display: block;
+    font-size: 1.375em;
+  }
+  .is-ghost {
+    background-color: white;
+    border: #3CAF81 3px solid;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
+  <div class="container">
     <h1>I bring ideas to reality</h1>
     <h2>My name is Juan<br> I'm a Web Developer <br/>and Web Designer</h2>
-    <img src="~/static/svg/mobile-wireframe.svg" alt="">
+    <div class="cta-btns">
+    <link-button text="See my Work" address="/dev" :isExternal="false"  />
+    <link-button text="Contact me" address="/contact" :isExternal="false" :isGhost="true"  />
+    </div>
+    <img class="hero-img" src="~/static/svg/mobile-wireframe.svg" alt="">
   </div>
 </template>
 
@@ -12,5 +16,37 @@
   }
 </script>
 
-<style>  
+<style>
+  .container {
+    padding: 0 1em;
+  }
+  h1 {
+    font-size: 3em;
+    font-size: clamp(2.5em, 5.5vw, 5em);
+    text-align: center;
+    text-transform: uppercase;
+    line-height: 2.5rem;
+    font-weight: bold;
+  }
+  h2 {
+    text-align: center;
+    font-weight: normal;
+    line-height: 100%;
+    font-size: clamp(1.625em, 3vw, 3.25em) ;
+  }
+  .hero-img {
+    width: 50%;
+    display: block;
+    margin: 4rem auto 0;
+  }
+  .cta-btns {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 auto;
+    width: fit-content;
+    gap: 2rem;
+    row-gap: 1rem;
+    justify-content: center;
+  }
+
 </style>
