7395db6254c06fb38b3800407f2425a511264535
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-05-28 03:50:18 -0400

    minimum styling in about me page

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <h2 class="heading-2">About</h2>
-    <p>
-    My name is Juan Pablo Echeverry, I am a software developer with a passion for coding and solving problems.<br/>
-    I mainly use frontend technologies such as HTML, CSS, JavaScript, and cool JS frameworks such as React, Next, Vue and Nuxt.<br/>
-    I also have implemented backend solutions developing APIs and setting up databases with PostgreSQL and MySQL.<br/>
-    When I'm not coding I love to learn about personal finances, and I love indoor rock climbing, haven't tried outdoor climbing yet!!
-    </p>
-    <nuxt-img src="images/profilePic.jpg" />
+    <div class="two-cols">
+      <p>
+      My name is Juan Pablo Echeverry, I am a software developer with a passion for coding and solving problems.<br/>
+      I mainly use frontend technologies such as HTML, CSS, JavaScript, and cool JS frameworks such as React, Next, Vue and Nuxt.<br/>
+      I also have implemented backend solutions developing APIs and setting up databases with PostgreSQL and MySQL.<br/>
+      When I'm not coding I love to learn about personal finances, and I love indoor rock climbing, haven't tried outdoor climbing yet!!
+      </p>
+      <nuxt-img class="profile-pic" src="images/profilePic.jpg" />
+    </div>
     <!-- TODO -->
     <!-- ADD gallery with 3 pics minimum in a grid -->
   </div>
@@ -19,6 +21,17 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+  .profile-pic {
+    max-width: 50%;
+  }
 
+
+  @media only screen and (min-width: 700px) {
+    .two-cols {
+      display: flex;
+      flex-basis: 0;
+      flex-grow: 1;
+    }
+  }
 </style>
