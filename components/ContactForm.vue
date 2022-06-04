8004397c065a784dbf9ef04cf864a21c991c8906
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-06-03 23:23:17 -0400

    replaced vanilla css for tailwindcss in contact form component

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="contact-section">
-    <h3>Say Hi</h3>
-    <form action="https://formsubmit.co/jpecheverryp@gmail.com" method="POST">
-      <div class="input-control">
-        <label for="username">Name*</label>
-        <input type="text" name="username" id="username" required>
+  <div class="text-center">
+    <h3 class="text-brand">Say Hi</h3>
+    <form class="w-fit mt-0 mx-auto mb-24" action="https://formsubmit.co/jpecheverryp@gmail.com" method="POST">
+      <div class="text-left">
+        <label class="block" for="username">Name*</label>
+        <input class="w-full py-2 px-3" type="text" name="username" id="username" required>
       </div>
-      <div class="input-control">
-        <label for="email">Email*</label>
-        <input type="email" name="email" id="email" required>
+      <div class="text-left">
+        <label class="block" for="email">Email*</label>
+        <input class="w-full py-2 px-3" type="email" name="email" id="email" required>
       </div>
-      <div class="input-control">
-        <label for="message">Message*</label>
-        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+      <div class="text-left">
+        <label class="block" for="message">Message*</label>
+        <textarea class="resize-y" name="message" id="message" cols="30" rows="10"></textarea>
       </div>
-      <input class="send-msg" type="submit" value="Send Message">
+      <input class="w-full py-2 px-3 bg-brand border-none rounded-md" type="submit" value="Send Message">
     </form>
   </div>
   
@@ -25,35 +25,3 @@
     
   }
 </script>
-
-<style scoped>
-  label {
-    display: block;
-  }
-  .contact-section {
-    text-align: center;
-  }
-  form {
-    width: fit-content;
-    margin: 0 auto 6rem;
-  }
-  .input-control {
-    text-align: left;
-  }
-  h3 {
-    color: #3CAF81;
-  }
-  input {
-    width: 100%;
-    padding: .5rem .75rem;
-  }
-  textarea {
-    resize: vertical;
-  }
-  .send-msg {
-    background-color: #3CAF81;
-    border: none;
-    border-radius: 5px;
-
-  }
-</style>
