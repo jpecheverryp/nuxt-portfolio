49797f94530e9ce5beeb0c50696edef8b5d3d3c3
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-06-03 21:37:53 -0400

    replaced vanilla css for tailwind in Footer component

---
+++ components/FooterLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <a target="_blank" :href="route">{{ text }}</a>
+</template>
+
+<script>
+  export default {
+    props: {
+      route: {
+        type: String,
+        required: true
+      },
+      text: {
+        type: String,
+        required: true
+      },
+    }
+  }
+</script>
+
+<style scoped>
+  
+  a {
+    text-decoration: none;
+    color: black;
+    background-image: linear-gradient(to bottom, #eee 50%, #7ED3B1 50%);
+    background-size: 100% 200%;
+    background-position: 0 40%;
+    transition: .4s;
+    
+  }
+  a:hover {
+    background-position: 0 90%;
+  }
+</style>
--- components/pageFooter.vue
+++ components/pageFooter.vue
@@ -1,8 +1,8 @@
 <template>
-  <footer>
-      <hr />
-      <br />
-      <p>This is an <a href="https://github.com/jpecheverryp/nuxt-portfolio" target="_blank">Open Source</a> Portfolio created with <a href="https://nuxtjs.org/" target="_blank">NuxtJS</a></p>
+  <footer class="text-center mb-16">
+      <hr class="w-4/5 mx-auto border-t-2 border-brand border-solid mb-6"/>
+      
+      <p >This is an <footer-link route="https://github.com/jpecheverryp/nuxt-portfolio" text="Open source" /> Portfolio created with <footer-link route="https://nuxtjs.org/" text="NuxtJS" />.</p>
       <p>&copy; {{new Date().getFullYear()}} Juan P Echeverry. All Rights Reserved.</p>
     </footer>
 </template>
@@ -13,30 +13,3 @@
   }
 </script>
 
-<style scoped>
-  footer {
-    text-align: center;
-    margin-bottom: 4em;
-  }
-  hr {
-    width: 80%;
-    border-top: 2px solid #3CAF81;
-  }
-  p {
-    padding: 0;
-    margin: 0;
-    line-height: 140%;
-  }
-  a {
-    text-decoration: none;
-    color: black;
-    background-image: linear-gradient(to bottom, #eee 50%, #7ED3B1 50%);
-    background-size: 100% 200%;
-    background-position: 0 40%;
-    transition: .4s;
-    
-  }
-  a:hover {
-    background-position: 0 90%;
-  }
-</style>
