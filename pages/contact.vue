8081f3f6d5b4af829bf269c2181f5a374c92707a
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-06-04 00:42:58 -0400

    refactor contact page css

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,32 +1,17 @@
 <template>
-  <div class="sect__contact">
-    <h2 class="text-3xl text-center">Contact me</h2>
-    <div class="text-center">
-      <p>Send me an email at <a href="mailto:jpecheverryp@gmail.com">jpecheverryp@gmail.com</a><br>Or use the form below </p>
-    </div>
+  <div>
+    <h2 class="text-3xl text-center">Contact me</h2>  
+    <p class="text-center">Send me an email at <a href="mailto:jpecheverryp@gmail.com">jpecheverryp@gmail.com</a><br>Or use the form below </p>
     <contact-form />
   </div>
 </template>
-<script>
 
+<script>
   export default {
     head() {
       return {
         title: '| Contact'
       }
     },
-    data() {
-      return {
-        linkedin: 'https://www.linkedin.com/in/jpe04/',
-        github: 'https://github.com/jpecheverryp',
-        email: 'mailto:jpecheverryp@gmail.com',
-      }
-    }
   }
 </script>
-
-<style>
-  .contact-text {
-    text-align: center;
-  }
-</style>
