eb92050c92aa6cae5d6cd5f71975e7cdd2f1b97e
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-06-06 01:31:57 -0400

    refactor index page style to make it look better

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <h2 class="text-3xl text-center">Contact me</h2>  
-    <p class="text-center">Send me an email at <a href="mailto:jpecheverryp@gmail.com">jpecheverryp@gmail.com</a><br>Or use the form below </p>
+    <h2 class="text-3xl text-center">Contact me</h2>
+    <p class="text-center">Send me an email at <a class="text-brand underline"
+        href="mailto:jpecheverryp@gmail.com">jpecheverryp@gmail.com</a><br>Or use the form below </p>
     <contact-form />
   </div>
 </template>
 
 <script>
-  export default {
-    head() {
-      return {
-        title: '| Contact'
-      }
-    },
-  }
+export default {
+  head() {
+    return {
+      title: '| Contact'
+    }
+  },
+}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,50 @@
 <template>
-<div class="page">
-  <main class="px-12 md:grid md:grid-cols-2 md:justify-between md:my-0 md:mx-auto md:max-w-7xl">
-    <div class="hero__content">
-      <h1 class="text-5xl text-center uppercase font-bold md:text-left">I bring ideas to reality</h1>
-      <p class="text-center font-normal text-2xl md:text-left">My name is Juan<br> I'm a Full Stack Developer <br/>and Web Designer</p>
-      <div class="flex flex-wrap my-6 mx-auto gap-8 gap-y-4 justify-center md:justify-start md:mt-24">
-        <nuxt-link-button text="See my Work" address="/dev" />
-        <nuxt-link-button text="Contact me" address="/contact" :isGhost="true"  />
+  <div class="px-8 max-w-5xl mx-auto">
+    <main class="md:grid md:grid-cols-2 md:justify-between md:my-0 md:mx-auto md:max-w-7xl">
+      <div class="hero__content h-screen flex flex-col justify-center ">
+        <div class="flex flex-col gap-6">
+          <h1 class="text-6xl text-center uppercase font-bold md:text-left">I'm <span class="text-brand">Juan</span> P
+            Echeverry</h1>
+          <p class="text-center font-normal text-2xl md:text-left">I'm a Full Stack Developer
+            <br />and Web Designer
+          </p>
+        </div>
+        <div class="flex flex-wrap my-6 gap-8 gap-y-4 justify-center  md:justify-start md:mt-24">
+          <nuxt-link-button text="See my Work" address="/dev" />
+          <nuxt-link-button text="Contact me" address="/contact" :isGhost="true" />
+        </div>
       </div>
-    </div>
-    <img class="hero-animation w-1/2 block mt-16 mx-auto mb-0" src="~/static/svg/mobile-wireframe.svg" alt="">
-  </main>
-  <work-section class="mt-36" heading="My Work" />
-  <h2 class="text-3xl text-center">Contact me</h2>
-  <contact-form />
-</div>
+      <img class="hidden md:block hero-animation w-1/2 mt-16 mx-auto mb-0" src="~/static/svg/mobile-wireframe.svg"
+        alt="">
+    </main>
+    <work-section class="" heading="My Work" />
+    <h2 class="text-3xl text-center">Contact me</h2>
+    <contact-form />
+  </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+
+}
 
 </script>
 
 <style>
-  @keyframes slideFromRight {
-    0% {
-      transform: translateX(50%);
-      opacity: 0;
-    }
-    100% {
-      transform: translateX(0);
-      opacity: 1;
-    }
+@keyframes slideFromRight {
+  0% {
+    transform: translateX(50%);
+    opacity: 0;
   }
-  .hero-animation {
-    animation: 1s ease-in-out 0s 1 slideFromRight;
-    transition: .5s;
+
+  100% {
+    transform: translateX(0);
+    opacity: 1;
   }
+}
+
+.hero-animation {
+  animation: 1s ease-in-out 0s 1 slideFromRight;
+  transition: .5s;
+}
 </style>
