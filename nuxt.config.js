bb530c4f1afddac7dcf55a99ff48322c9f63c06a
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-03-28 22:40:38 -0400

    update dependencies and remove nuxt content

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,46 @@
 export default {
-  components: true,
-  target: 'static',
-  head: {
-    titleTemplate: 'Juan P Echeverry %s',
-    htmlAttrs: {
-      lang: 'en'
+    components: true,
+    target: 'static',
+    head: {
+        titleTemplate: 'Juan P Echeverry %s',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'My Personal Portfolio'
+            }
+        ],
+        link: [
+            {
+                rel: 'icon',
+                type: 'image/x-icon',
+                href: '/favicon.ico'
+            }
+        ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { 
-        hid: 'description',
-        name: 'description',
-        content: 'My Personal Portfolio'
-      }
+    buildModules: [
+        '@nuxtjs/google-fonts'
     ],
-    link: [
-      {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico' 
-      }
-    ]
-  },
-  modules: [
-    ['nuxt-content-git', {
-      createdAtName: 'gitCreatedAt',
-      updatedAtName: 'gitUpdatedAt',
-    }],
-    '@nuxt/content',
-    '@nuxt/image',
-    '@nuxtjs/tailwindcss'
-  ],
-  buildModules: [
-    '@nuxtjs/google-fonts',
-  ],
-  css: ['~/assets/css/style.css'],
-  googleFonts: {
-    families: {
-      'Poppins': [400, 700]
+    css: ['@/assets/css/main.css'],
+    googleFonts: {
+        families: {
+            'Poppins': [400, 700]
+        }
+    },
+    devServerHandlers: [],
+    build: {
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                }
+            }
+        }
     }
-  },
-  devServerHandlers: [],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {
-      colors: {
-        'brand': '#3CAF81'
-      }
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'brand': '#3CAF81'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
